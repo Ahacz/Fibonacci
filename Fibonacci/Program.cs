dfe169796643946ecb48b6b9d72bf23f1b63c00a
Michau <Michau@DESKTOP-53TJ0BT>
2024-07-13 20:51:58 +0200

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Write a function calculating the number in fibonacci sequence that accepts a natural number as the nth number of the series to calculate.
